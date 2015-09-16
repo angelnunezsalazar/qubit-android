b116d13db2eb01d72838ef4cc40a37bddd461518
Snahider <snahider@gmail.com>
2015-09-16 02:20:18 -0500

    assert videoplayer by class

--- spec/videoplayer_spec.rb
+++ spec/videoplayer_spec.rb
@@ -3,19 +3,31 @@ require 'spec_helper'
 
 describe 'VideoPlayer' do
 
-  it 'should watch movies on the video player' do
+  it 'should play movies' do
     #Login
-    find_element(:id, 'tv.qubit.android.player:id/acceptButton').click
     find_element(:id, 'tv.qubit.android.player:id/menuBarMoreButton').click
     find_element(:name, 'Iniciar sesión').click
     find_element(:id, 'tv.qubit.android.player:id/userNameEditText').send_keys 'qa2'
     find_element(:id, 'tv.qubit.android.player:id/passwordEditText').send_keys '000000'
     find_element(:id, 'tv.qubit.android.player:id/loginButton').click
-    sleep(5) #Reemplazar
+    sleep(5)
 
+    #Lista de Películas
     videos= find_element(:class, 'android.widget.ListView').find_elements(:class, 'android.widget.LinearLayout')
     first_video=videos[0].find_element(:class, 'android.widget.RelativeLayout')
-    price=first_video.find_element(:id,'tv.qubit.android.player:id/playLinearLayout').find_element(:id,'tv.qubit.android.player:id/mediaRowPlayButton')
-    price.click
+    show_button=first_video.find_element(:id,'tv.qubit.android.player:id/playLinearLayout').find_element(:id,'tv.qubit.android.player:id/mediaRowPlayButton')
+    show_button.click
+    sleep(10)
+
+    #Popup ¿Deseas Reanudar?
+    popup=find_elements(:id, 'tv.qubit.android.player:id/messageText')
+    if popup.size > 0
+        find_element(:id, 'tv.qubit.android.player:id/messageCancelButton').click
+    end
+
+    #Player
+    wait = Selenium::WebDriver::Wait.new(:timeout => 120)
+    video_player=wait.until {find_element(:id,'tv.qubit.android.player:id/video')}
+    expect(video_player).not_to be_nil 
   end
 end
